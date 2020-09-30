449e28be401e96c3252aeba3b0c9f2d87bf1cdb6
kelvintandrio <kelvin.tandrio@tsmid.com>
2020-09-30 18:36:48 +0700

    Success new project SpringBoot using Java

---
+++ TrainingJavaSpringBoot/src/main/java/algokelvin/springboot/java/demo/DemoSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package algokelvin.springboot.java.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class DemoSpringBootJavaApplication {
+
+	public static void main(String[] args) {
+		run(DemoSpringBootJavaApplication.class, args);
+	}
+
+}

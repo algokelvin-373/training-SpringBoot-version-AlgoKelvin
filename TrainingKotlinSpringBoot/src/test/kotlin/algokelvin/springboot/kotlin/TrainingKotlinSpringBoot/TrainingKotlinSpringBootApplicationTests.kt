17b136c7cbe6fb80bb1b9d0d02f16397ed9f6bd7
kelvin373-ht <kelvinht373@gmail.com>
2020-10-15 23:39:25 +0700

    SpringBoot Kotlin: Success New SpringBoot using Kotlin

---
+++ TrainingKotlinSpringBoot/src/test/kotlin/algokelvin/springboot/kotlin/TrainingKotlinSpringBoot/TrainingKotlinSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package algokelvin.springboot.kotlin.TrainingKotlinSpringBoot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrainingKotlinSpringBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

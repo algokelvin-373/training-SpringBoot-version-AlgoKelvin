2c27e96dbfcedb290565ac0f6a5e895958d5b23f
kelvintandrio <kelvin.tandrio@tsmid.com>
2020-10-15 21:06:44 +0700

    Make method POST {base_url}/algokelvin/message/post

---
+++ TrainingJavaSpringBoot/src/main/java/algokelvin/springboot/java/demo/DemoPost.java
@@ -0,0 +1,20 @@
+package algokelvin.springboot.java.demo;
+
+import algokelvin.springboot.java.demo.model.Results;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoPost {
+    //Using PostMapping for POST API
+    @PostMapping(value = "/algokelvin/message/post", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Results> responseMessage() {
+
+        Results results = new Results(200, "Success Call API POST");
+
+        return new ResponseEntity<>(results, HttpStatus.OK);
+    }
+}
